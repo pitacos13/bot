2f9b22fcf2476c389618f17b8727c5a173bd3870
Dennycbo <94752076+Dennycbo@users.noreply.github.com>
2022-03-31 02:22:35 -0300

    Update BlazeRoyaleR.js

--- models/BlazeRoyaleR.js
+++ models/BlazeRoyaleR.js
@@ -6,7 +6,8 @@ let BlazeRoyaleR = new mongoose.model("BlazeRoyaleR", {
     plan_id:String,
     max_date:String,
     min_date:String,
-    page_find:String
+    page_find:String,
+    date_payment:String
 })
 
 module.exports = BlazeRoyaleR;
