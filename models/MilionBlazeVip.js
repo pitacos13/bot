b768d385613035e75b9b34c84b99bb051d1eef30
Dennycbo <94752076+Dennycbo@users.noreply.github.com>
2022-03-31 02:23:12 -0300

    Update MilionBlazeVip.js

--- models/MilionBlazeVip.js
+++ models/MilionBlazeVip.js
@@ -6,7 +6,8 @@ let MilionBlazeVip = new mongoose.model("MilionBlazeVip", {
     plan_id:String,
     max_date:String,
     min_date:String,
-    page_find:String
+    page_find:String,
+    date_payment:String
 })
 
 module.exports = MilionBlazeVip;
