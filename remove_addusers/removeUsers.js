916adc005258e805647139eb2af961aabe37d591
Dennycbo <94752076+Dennycbo@users.noreply.github.com>
2022-04-10 23:37:01 -0300

    Update removeUsers.js

--- remove_addusers/removeUsers.js
+++ remove_addusers/removeUsers.js
@@ -9,33 +9,73 @@ async function removeUsersPending(){
     const myUsers = require("../models/Users")
     let usersRecived = await myUsers.find()
     for(let i=0; i<usersRecived.length; i++){
-      const userMail = usersRecived[i].email_user
-      const userId = usersRecived[i].user_id
-      const planName = usersRecived[i].plan_name
-      let active = Boolean;
+      const user_mail = usersRecived[i].email_user
+      const user_id = usersRecived[i].user_id
+      const plan = usersRecived[i].plan_name
+      let status = Boolean;
          
         const Plans = require(`../models/${planName}`)
         let find_User = await Plans.findOne({email_user:userMail})   
         if(find_User == null){
           //Remove
-          active = false
+          status = false
           console.log("User not finded")
-           await kikeUserOrAdd(userMail, userId, active, planName)
+   ///// VAI DESBANIR O MEMBRO NO RESPECTIVO PLANO E ENVIAR UM CONVITE DE ACORDO COM O PLANO E GRUPO
+  // VAMOS USAR UM FOR PARA PEGAR O NOME DO PLANO E O EMAIL, PARA CONSEGUIRMOS CAPTALO NO GP
+  let myUsers;
+  try{
+    myUsers = require("../models/Users")
+  }catch(e){
+    myUsers = require("./models/Users")
+  }
+  const user = await myUsers.findOneAndUpdate({email_user:user_mail, plan_name:plan}, {status_plan:status}) // FindOne não, aqui é outro metodo.
+  let plans = {MilionBlazeR:-1001503352913, BlazeRoyale:-1001688857780,BlazeRoyaleR:-1001688857780, MilionBlazeVip:-1001503352913, StarCrash:-1001592231367}
+  let plans_invite = {MilionBlazeR:"https://t.me/+o5-YgmuIYuQwZjRh", BlazeRoyale:"https://t.me/+3oPIfRRG8tgzN2Jh",BlazeRoyaleR:"https://t.me/+3oPIfRRG8tgzN2Jh", MilionBlazeVip:"https://t.me/+o5-YgmuIYuQwZjRh", StarCrash:"https://t.me/+sipUKfOsV-JlN2Vh"}
+  status == false?(async()=>{
+    let oneStatusTrue = false;
+    const userToFind = await myUsers.find({email_user:user_mail})
+    for(let ele of userToFind){
+      if(ele.status_plan == true){
+        oneStatusTrue = true
+      }else{
+        
+      }
+  }
+  if(oneStatusTrue == true){
+    console.log(plans[plan])
+    bot.telegram.banChatMember(plans[plan], user_id)
+  }else{
+    // Banir do grupo respectivo Verificaremos se o email do usuario ainda consta no Users, caso conste, mantenha ele no StarCrash 
+    // Caso contrario, remova ele do StarCrashs [Localizar todos registro do usuario, e todos status, caso um esteja ativo, mantenha-o]
+    console.log(plans[plan])
+    bot.telegram.banChatMember(plans[plan], user_id) // Grupo
+    bot.telegram.banChatMember(plans["StarCrashs"], user_id) //StarCrashs
+  }
+  })():(()=>{
+    try{
+      bot.telegram.getChatMember(plans[plan], user_id)
+      .then((e)=>{
+      e.status == "kicked"?(()=>{
+      bot.telegram.unbanChatMember(plans[plan], user_id) // Grupo
+      bot.telegram.unbanChatMember(plans["StarCrashs"], user_id) // StarCrash
+      // Grupo 
+      bot.telegram.sendMessage(user_id, plans_invite[plan])
+      // StarCrash // Se o star Crash Já foi usado 1 vez, e o email for igual, evitamos enviar ele novamente
+      bot.telegram.sendMessage(user_id, plans_invite["StarCrashs"])
+        })():(()=>{
+          throw("User not removed");
+        })();
+      }).catch((e)=>{
+        console.log(e)
+      })
+    }catch(e){
+      console.log("user not removed")
+    }
+  })();
         }
         else{
           // Encontrou
-           active = true
-           await kikeUserOrAdd(userMail, userId, active, planName)
-        }
-    }
-    const path = require("path")
-    let status = require(path.join(__dirname, "../models/Verification"))
-    await status.findOneAndRemove({running:true})
-}
-
-let emailsToSave = [] // Salvar emails e evitar duplicidade do starcrash
-
-async function kikeUserOrAdd(user_mail, user_id, status, plan){
+           status = true
   ///// VAI DESBANIR O MEMBRO NO RESPECTIVO PLANO E ENVIAR UM CONVITE DE ACORDO COM O PLANO E GRUPO
   // VAMOS USAR UM FOR PARA PEGAR O NOME DO PLANO E O EMAIL, PARA CONSEGUIRMOS CAPTALO NO GP
   let myUsers;
@@ -88,6 +128,17 @@ async function kikeUserOrAdd(user_mail, user_id, status, plan){
       console.log("user not removed")
     }
   })();
+        }
+    }
+    const path = require("path")
+    let status = require(path.join(__dirname, "../models/Verification"))
+    await status.findOneAndRemove({running:true})
+}
+
+let emailsToSave = [] // Salvar emails e evitar duplicidade do starcrash
+
+async function kikeUserOrAdd(user_mail, user_id, status, plan){
+
 }
 module.exports = removeUsersPending
 removeUsersPending();
