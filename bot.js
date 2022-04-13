02a2c166b9615abbacb6f3c809cfe01f75ec8d45
Dennycbo <94752076+Dennycbo@users.noreply.github.com>
2022-04-12 21:54:38 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ const app = express()
 process.env.TZ = 'America/Sao_Paulo';
 const verification = require("./models/Verification")
 setInterval(async()=>{
-  if(new Date(Date.now()).toLocaleTimeString("pt-BR") == "21:51:10"){
+  if(new Date(Date.now()).toLocaleTimeString("pt-BR") == "21:55:20"){
     await verification.create({running:true})
     const updateDb = require("./update_db/CaptureStatus")
     updateDb()
