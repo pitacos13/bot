7b9de4d024f832a826874d96cc89d8cd0308d3f3
Dennycbo <94752076+Dennycbo@users.noreply.github.com>
2022-04-14 11:07:20 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,10 +24,11 @@ setInterval(async()=>{
   let allUsers = await users.find({})
   let groupsOfUsers = [-1001688857780, -1001503352913, -1001592231367]
   for(let user of allUsers){
-    console.log("EXECUTEI")
+    console.log(user)
     for(let group of groupsOfUsers){
+      console.log(group, user.user_id)
       try{
-        await bot.telegram.unbanChatMember(group, user.user_id)
+        bot.telegram.unbanChatMember(group, user.user_id)
       }catch(e){
         console.log("NAO BANIDO")
       }
