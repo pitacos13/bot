1b7f361aeb67b2b9d1354bebb0b42b4a228493e4
Dennycbo <94752076+Dennycbo@users.noreply.github.com>
2022-04-18 07:07:21 -0700

    Update Users.js

--- models/Users.js
+++ models/Users.js
@@ -4,7 +4,8 @@ let Users = new mongoose.model("Users", {
     user_id:Number,
     email_user:String,
     plan_name:String,
-    status_plan:Boolean
+    status_plan:Boolean,
+    recived:Boolean
 })
 
 module.exports = Users;
