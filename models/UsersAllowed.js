a34f2f4c0d59f12368dc90fe1dd2e03295b6e62b
Dennycbo <94752076+Dennycbo@users.noreply.github.com>
2022-04-21 05:47:18 -0700

    Update UsersAllowed.js

--- models/UsersAllowed.js
+++ models/UsersAllowed.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose")
 
 let UsersAllowed = new mongoose.model("UsersAllowed", {
     user_id:Number,
-    user_name:String
+    user_name:String,
+    recived:Boolean
 })
 
 module.exports = UsersAllowed;
